6774b15391e04e3e35216cc23ed9f06c75b3a999
zakrzu1899 <143639964+zakrzu1899@users.noreply.github.com>
2023-08-31 19:17:45 +0200

    Add files via upload
    changes in JS

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
 const burger = document.querySelector('.burger');
 const navMobile = document.querySelector('.navbar-mobile-menu');
-const navMobileLinkHome = document.querySelector('.nav-mobile-link-home');
-const navMobileLinkAbout = document.querySelector('.nav-mobile-link-about');
-const navMobileLinkOffer = document.querySelector('.nav-mobile-link-offer');
+const navMobileLink = document.querySelectorAll('.nav-mobile-link');
 const msgStatus = document.querySelector('.msg-status');
+
+const navMobileLinkSpread = [...navMobileLink];
+
+// const section = document.querySelectorAll('section');
+// const navLinksDesktop = document.querySelectorAll('.nav-items a');
+
+// window.onscroll = () => {
+// 	section.forEach((sec) => {
+// 		let top = window.scrollY;
+// 		let offset = sec.offsetTop;
+// 		let height = sec.offsetHeight;
+// 		let id = sec.getAttribute('id');
+// 		if (top >= offset && top < offset + height) {
+// 			navLinksDesktop.forEach(links => {
+// 				links.classList.remove('active');
+// 				document.querySelector('.nav-items a[href*=' + id + ']').classList.add('active');
+// 			})
+// 		}
+// 	});
+// };
 
 burger.addEventListener('click', () => {
 	navMobile.classList.toggle('open');
 	burger.classList.toggle('open');
 });
 
-navMobileLinkHome.addEventListener('click', () => {
-	navMobile.classList.remove('open');
-	burger.classList.remove('open');
+navMobileLinkSpread.forEach((item) => {
+	item.addEventListener('click', () => {
+		item.closest('nav').classList.remove('open');
+		burger.classList.remove('open');
+	});
 });
-
-navMobileLinkAbout.addEventListener('click', () => {
-	navMobile.classList.remove('open');
-	burger.classList.remove('open');
-});
-
-navMobileLinkOffer.addEventListener('click', () => {
-	navMobile.classList.remove('open');
-	burger.classList.remove('open');
-});
-
-
 
 // kod do formularza, który nie działa bo nie jest podpięty na hosting
 
